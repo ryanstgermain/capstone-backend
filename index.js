eb520c634146e0305524d3d334a9d4f470db75ec
ryanstgermain <ryanjoel26@gmail.com>
2019-01-28 21:01:40 -0700

    installed cors, bodyparser, express, knex, setup requests, and knexfile, and added a start script

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+//const queries = require('./queries.js');
+const knex = require('knex');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    queries.dbQueryGetAll().then(response => res.send(response))
+})
+
+app.get('/:id', (req, res) => {
+    queries.dbQueryGetById(req.params.id).then(movies => res.send(movies))
+})
+
+app.post('/', (req, res) => {
+    queries.dbQueryCreateMovie(req.body).then(movies => res.send(movies))
+})
+
+app.delete('/:id', (req, res) => {
+    queries.deleteMovie(req.params.id).then(res.status(204).send())
+})
+
+app.put('/:id', (req, res) => {
+    queries.dbQueryUpdateMovie(req.params.id, req.body).then(updatedMovieInfo => res.json(updatedMovieInfo[0]))
+})
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+app.listen(port, function() {
+    console.log(`Listening on ${port}`);
+})
+
+module.exports = app
+
---
+++ knexfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+    
+development: {
+    client: 'pg',
+    connection: 'postgresql://localhost/movies'
+},
+
+production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+}
+
+};
